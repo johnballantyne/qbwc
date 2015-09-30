f779b678ae581a5137173c27616a19c6ad80973c
John <johnballantyne@gmail.com>
2015-09-29 23:43:08 -0400

    Lightweight functional SOAP client

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 var soap = require('soap');
 
+var url = 'http://localhost:8000/wsdl?wsdl';
+
+soap.createClient(url, function(err, client) {
+    if (err) {
+        console.log('Error creating SOAP client');
+        console.log('Error!');
+    }
+
+    client.serverVersion({}, function(err, result) {
+        if (err) {
+            console.log('Error calling serverVersion()');
+            console.log(err);
+        }
+        console.log(result);
+    });
+});
+
